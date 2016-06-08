8461baad66d1b0ab28d459a3ca7ae5729c1e7027
Vladimir Kopylov <vkopylov@cs-cart.com>
2016-06-08 12:35:16 +0400

    [*] {@16928} Csv parsing logic moved to the Client.

--- classes/SitesChecker/Client.php
+++ classes/SitesChecker/Client.php
@@ -15,6 +15,8 @@ class Client extends \GearmanClient
     protected $output_file_name;
     
     protected $input_file_handle;
+    
+    protected $host_black_list = array('none', 'null', 'localhost');
 
     public function __construct($input_file_name, $output_file_name)
     {
@@ -30,8 +32,10 @@ class Client extends \GearmanClient
     {
         $this->openInputFile();
         
-        $this->addTasksFromInput();
-        
+        while (($data = fgets($this->input_file_handle, self::READ_BUFF_SIZE)) !== false) {        
+            $this->addTasksFromRow($data);
+        }
+            
         $this->closeInputFile();
     }
 
@@ -45,17 +49,92 @@ class Client extends \GearmanClient
         //Skip header
         fgets($this->input_file_handle, self::READ_BUFF_SIZE);
     }
-
-    protected function addTasksFromInput()
+    
+    protected function addTasksFromRow($data)
     {
+        $input_sites_list = explode(',',$data);
 
-        while (($data = fgets($this->input_file_handle, self::READ_BUFF_SIZE)) !== false) {        
-            $this->addTask('checkSite', $data);
+        $task_sites_list = array();
+
+        foreach ($input_sites_list as $site) {
+
+            $site = $this->normalizeUrl($site);
+            if (!is_null($site) && !in_array($site, $task_sites_list)) {
+                $task_sites_list[] = $site;
+            }
         }
+
+        if (count($task_sites_list) > 0) {
+            $task_sites_list = $this->removeWwwDuplicates($task_sites_list);
+            
+        }
+        
+        foreach ($input_sites_list as $site) {
+                $this->addTask('checkSite', $site);
+        }         
     }
+
+    protected function normalizeUrl($site)
+    {
+        $result = rtrim(trim($site), '/?');
+
+        if (empty($result)) {
+            return null;
+        }
+
+        $is_url = $this->isValidUrl($result);
+        $is_host = !$is_url && $this->isValidHost($result);
+        
+        if (!$is_url && !$is_host) {
+            //FIXME: remove direct output in this class
+            echo "$result  is not a valid url or domain name\n";
+            return null;
+        }
+        
+        if ($is_host) {
+            $result = 'http://' . $result;
+        }
+
+        $host = parse_url($result, PHP_URL_HOST);
+        if (in_array(mb_strtolower($host), $this->host_black_list)) {
+            echo "$result's  host is in the black list\n";
+            return null;
+        }
+        
+        return $result;
+    }
+    
+    protected function isValidUrl($url) 
+    {
+        return preg_match('@https?://([^\s/?\.#]+\.?)+(/[^\s]*)?$@iS', $url);
+    }
+    
+    protected function isValidHost($name)
+    {
+        return preg_match('@([^\s/?\.#]+\.?)+(/[^\s]*)?$@iS', $name);
+    }
+    
+    protected function removeWwwDuplicates($sites_list)
+    {
+        foreach ($sites_list as $key => $site) {
+            $host = parse_url($site, PHP_URL_HOST);
+            if (mb_strpos($host, 'www.') === 0) {
+                continue;
+            }
+            $www_host = 'www.' . $host;
+            $www_site = str_replace($host, $www_host, $site);
+            if (in_array($www_site, $sites_list)) {
+                echo "$site is a duplicate in  " . implode(',', $sites_list) . "\n";
+                unset($sites_list[$key]);
+            }
+        }
+        return $sites_list;
+    } 
+    
+    
 
     protected function closeInputFile()
     {
         fclose($this->input_file_handle);
-    }
+    }    
 }
--- classes/SitesChecker/Worker.php
+++ classes/SitesChecker/Worker.php
@@ -8,8 +8,8 @@ namespace SitesChecker;
  */
 class Worker extends \GearmanWorker
 {
-
-    protected $host_black_list = array('none', 'null', 'localhost');
+    
+    protected $curl_resource = null;
 
     public function __construct()
     {
@@ -20,80 +20,23 @@ class Worker extends \GearmanWorker
 
     public function checkSite($job)
     {
-        $input_sites_list = explode(',', $job->workload());
-
-        $task_sites_list = array();
-
-        foreach ($input_sites_list as $site) {
-
-            $site = $this->normalizeUrl($site);
-            if (!is_null($site) && !in_array($site, $task_sites_list)) {
-                $task_sites_list[] = $site;
-            }
-        }
-
-        if (count($task_sites_list) > 0) {
-            $task_sites_list = $this->removeWwwDuplicates($task_sites_list);
-            //echo implode(',', $task_sites_list) . "\n";
-        }
+        echo $job->workload() . "\n";
 
         return true;
     }
-
-    protected function normalizeUrl($site)
-    {
-        $result = rtrim(trim($site), '/?');
-
-        if (empty($result)) {
-            return null;
-        }
-
-        $is_url = $this->isValidUrl($result);
-        $is_host = !$is_url && $this->isValidHost($result);
-        
-        if (!$is_url && !$is_host) {
-            //FIXME: remove direct output in this class
-            echo "$result  is not a valid url or domain name\n";
-            return null;
-        }
-        
-        if ($is_host) {
-            $result = 'http://' . $result;
-        }
-
-        $host = parse_url($result, PHP_URL_HOST);
-        if (in_array(mb_strtolower($host), $this->host_black_list)) {
-            echo "$result's  host is in the black list\n";
-            return null;
-        }
-        
-        return $result;
-    }
     
-    protected function isValidUrl($url) 
+    protected function initCurlSession()
     {
-        return preg_match('@https?://([^\s/?\.#]+\.?)+(/[^\s]*)?$@iS', $url);
-    }
-    
-    protected function isValidHost($name)
-    {
-        return preg_match('@([^\s/?\.#]+\.?)+(/[^\s]*)?$@iS', $name);
-    }
-    
-    protected function removeWwwDuplicates($sites_list) 
-    {
-        foreach ($sites_list as $key => $site) {
-            $host = parse_url($site, PHP_URL_HOST);
-            if (mb_strpos($host, 'www.') === 0) {
-                continue;
-            }
-            $www_host = 'www.' . $host;
-            $www_site = str_replace($host, $www_host, $site);
-            if (in_array($www_site, $sites_list)) {
-                echo "$site is a duplicate in  ". implode(',', $sites_list) . "\n";
-                unset($sites_list[$key]);
-            }
-        }
-        return $sites_list;
+
+        $resource = curl_init();
+
+        curl_setopt($resource, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($resource, CURLOPT_LOW_SPEED_LIMIT, 1);
+        curl_setopt($resource, CURLOPT_LOW_SPEED_TIME, self::TIMEOUT);
+        curl_setopt($resource, CURLOPT_FAILONERROR, true);
+        curl_setopt($resource, CURLOPT_RETURNTRANSFER, 1);
+
+        $this->curl_resource = $resource;
+
     }
 }
