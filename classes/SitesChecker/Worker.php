a02839d2bcc468a50f7e69878dcb8fb21052b976
Vladimir Kopylov <vkopylov@cs-cart.com>
2016-06-08 16:23:55 +0400

    [+] {@16928} Worker: Checking for cs-cart by /?version request implemented

--- classes/SitesChecker/Worker.php
+++ classes/SitesChecker/Worker.php
@@ -8,8 +8,17 @@ namespace SitesChecker;
  */
 class Worker extends \GearmanWorker
 {
+
+    const TIMEOUT = 30;
+    const ERROR_OK = 0;
+    const ERROR_NETWORK = 1;
+    const ERROR_NOTCS = 1;
     
-    protected $curl_resource = null;
+    protected $site = null;
+
+    protected $error_code = null;
+    protected $product_version = null;
+    protected $error_message = null;
 
     public function __construct()
     {
@@ -20,11 +29,39 @@ class Worker extends \GearmanWorker
 
     public function checkSite($job)
     {
-        echo $job->workload() . "\n";
+        $this->site = $job->workload();
+        $this->product_version = null;
+        $this->error_code = self::ERROR_OK;
+        $this->error_message = null;
+        $this->checkByVersionReq();
 
-        return true;
+        return $this->serializeResult();
     }
-    
+
+    protected function checkByVersionReq()
+    {
+
+        $result = false;
+        
+        $curl_resource = $this->initCurlSession();
+
+        $url = $this->site . '/?version';
+        curl_setopt($curl_resource, CURLOPT_URL, $url);
+
+        $curl_result = curl_exec($curl_resource);
+
+        if ($curl_result === false) {
+            $this->error_code = self::ERROR_NETWORK;
+            $this->error_message = curl_error($curl_resource);
+        } else {
+            $result = $this->checkVersionText($curl_result);
+        }
+
+        curl_close($curl_resource);
+        
+        return $result;
+    }
+
     protected function initCurlSession()
     {
 
@@ -36,7 +73,30 @@ class Worker extends \GearmanWorker
         curl_setopt($resource, CURLOPT_FAILONERROR, true);
         curl_setopt($resource, CURLOPT_RETURNTRANSFER, 1);
 
-        $this->curl_resource = $resource;
+        return $resource;
+    }
 
+    protected function checkVersionText($text)
+    {        
+        $text = trim($text);
+        $result = mb_stripos($text, 'CS-Cart') === 0 || mb_stripos($text, 'Multi-Vendor') === 0;
+        if ($result) {
+            $this->product_version = strip_tags($text);
+        } else {
+            $this->error_code = self::ERROR_NOTCS;
+        }
+        return $result;
+    }
+
+    protected function serializeResult()
+    {
+
+        $result = new \stdClass();
+        $result->site = $this->site;
+        $result->error_code = $this->error_code;
+        $result->product_version = $this->product_version;
+        $result->error_message = $this->error_message;
+
+        return json_encode($result);
     }
 }
