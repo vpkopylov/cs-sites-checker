57033ed7be43615ccecaa8294ee9576d8ce4e7bd
Vladimir Kopylov <vkopylov@cs-cart.com>
2016-06-09 12:55:17 +0400

    [~] {@16928} Minor formatting fixes.

--- classes/SitesChecker/Client.php
+++ classes/SitesChecker/Client.php
@@ -4,9 +4,9 @@ namespace SitesChecker;
 
 use SitesChecker\Worker;
 
-
 class Client extends \GearmanClient
 {
+
     const READ_BUFF_SIZE = 1000;
 
     protected $input_file_name;
@@ -18,14 +18,11 @@ class Client extends \GearmanClient
     protected $output_file_handle;
     
     protected $delim = ',';
-
-
+    
     protected $column_names = array(
         'site_url',
         'version'
     );
-
-
     protected $host_black_list = array(
         'none',
         'null',
@@ -33,7 +30,6 @@ class Client extends \GearmanClient
         'tbd',
         'tbn'
     );
-    
     protected $completed_callback = null;
     
     protected $tasks_total = null;
@@ -44,20 +40,19 @@ class Client extends \GearmanClient
     {
         $this->input_file_name = $input_file_name;
         $this->output_file_name = $output_file_name;
-        
+
         parent::__construct();
 
         $this->addServer();
     }
-    
-    public function setCompleteCallback($callback) 
+
+    public function setCompleteCallback($callback)
     {
         $this->completed_callback = $callback;
-        
+
         parent::setCompleteCallback(array($this, 'onTaskComplete'));
-        
     }
-    
+
     public function onTaskComplete($task)
     {
         $result_data = json_decode($task->data());
@@ -65,33 +60,34 @@ class Client extends \GearmanClient
             $put_data = array($result_data->site, $result_data->product_version);
             fputs($this->output_file_handle, implode($this->delim, $put_data) . "\n");
         }
-        
+
         $this->tasks_left--;
-        
+
         call_user_func($this->completed_callback, $task);
-    }    
+    }
 
     public function createTasks()
     {
         $this->openInputFile();
-        
+
         $this->createOutputFile();
-        
-        while (($data = fgets($this->input_file_handle, self::READ_BUFF_SIZE)) !== false) {        
+
+        while (($data = fgets($this->input_file_handle, self::READ_BUFF_SIZE)) !== false) {
             $this->addTasksFromRow($data);
         }
-            
+
         $this->closeInputFile();
     }
-    
-    public function tasksLeft() 
+
+    public function tasksLeft()
     {
         return $this->tasks_left;
     }
-    
-    public function tasksTotal() {
-        
-     return $this->tasks_total;
+
+    public function tasksTotal()
+    {
+
+        return $this->tasks_total;
     }
 
     protected function openInputFile()
@@ -104,7 +100,7 @@ class Client extends \GearmanClient
         //Skip header
         fgets($this->input_file_handle, self::READ_BUFF_SIZE);
     }
-    
+
     protected function addTasksFromRow($data)
     {
         $input_sites_list = explode($this->delim, $data);
@@ -142,11 +138,11 @@ class Client extends \GearmanClient
 
         $is_url = $this->isValidUrl($result);
         $is_host = !$is_url && $this->isValidHost($result);
-        
+
         if (!$is_url && !$is_host) {
             return null;
         }
-        
+
         if ($is_host) {
             $result = 'http://' . $result;
         }
@@ -155,20 +151,20 @@ class Client extends \GearmanClient
         if (in_array(mb_strtolower($host), $this->host_black_list)) {
             return null;
         }
-        
+
         return $result;
     }
-    
-    protected function isValidUrl($url) 
+
+    protected function isValidUrl($url)
     {
         return preg_match('@https?://([^\s/?\.#]+\.?)+(/[^\s]*)?$@iS', $url);
     }
-    
+
     protected function isValidHost($name)
     {
         return preg_match('@([^\s/?\.#]+\.?)+(/[^\s]*)?$@iS', $name);
     }
-    
+
     protected function removeWwwDuplicates($sites_list)
     {
         foreach ($sites_list as $key => $site) {
@@ -183,20 +179,20 @@ class Client extends \GearmanClient
             }
         }
         return $sites_list;
-    } 
-        
+    }
+
     protected function closeInputFile()
     {
         fclose($this->input_file_handle);
-    }   
-    
-    protected function createOutputFile() {
-      
+    }
+
+    protected function createOutputFile()
+    {
+
         $this->output_file_handle = @fopen($this->output_file_name, 'w');
         if (!$this->output_file_handle) {
             throw new Exception('Unable to create ' . $this->output_file_name);
-        }        
+        }
         fputs($this->output_file_handle, implode($this->delim, $this->column_names) . "\n");
     }
-    
 }
