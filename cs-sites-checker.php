0dbaf7604aa14d7a4ac8425ea6b198be6cd06c08
Vladimir Kopylov <vkopylov@cs-cart.com>
2016-06-09 12:15:17 +0400

    [*] {@16928} Exception's handling removed from the utility.

--- cs-sites-checker.php
+++ cs-sites-checker.php
@@ -2,19 +2,14 @@
 <?php
 $loader = require __DIR__ . '/vendor/autoload.php';
 $loader->add('SitesChecker', __DIR__ . '/classes');
-
 use SitesChecker\Console\RunCommand;
 use SitesChecker\Console\WorkerCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
-try {
-    $application->add(new RunCommand);
-    $application->add(new WorkerCommand);
-    $application->run();
-} catch (\Exception $e) {
-    $message = 'Fatal error: ' . $e->getMessage();
-    echo $message . "\n";
-    return false;
-}
+
+$application->add(new RunCommand);
+$application->add(new WorkerCommand);
+$application->run();
+
 return true;
